ebcc9530deef10b4d1120ef179b5f222475aa64f
Can LO <43052031+canlo33@users.noreply.github.com>
2021-03-17 18:45:58 -0400

    Started Working On PlayerInput and PlayerController Scripts
    1)Player model and some player animations are added to the project.
    
    2)Started working on PlayerInput Script which will be handling the input taken from the player.
    
    3)Started working on PlayerController Script which will handle the functionalities such as Movement, Dash, Attack, Dodge etc.
    
    4) Implemented Dash functionality. But still need to add particle effect on player to activate while dashing.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float dashTimer;
+    public float dashCooldown;
+    private bool isDashing;
+    public float dashSpeed;
+    private Animator anim;
+    private Rigidbody rb;
+    private PlayerInput playerInput;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponentInChildren<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        AnimatorHandler();
+        Dash();
+        Movement();
+    }
+    void AnimatorHandler()
+    {
+        anim.SetFloat("horizontalMove", playerInput.HorizontalMove);
+        anim.SetFloat("verticalMove", playerInput.VerticalMove);
+        if (!isDashing)
+        {
+            anim.SetFloat("horizontalMoveRaw", playerInput.HorizontalMoveRaw);
+            anim.SetFloat("verticalMoveRaw", playerInput.VerticalMoveRaw);
+        }
+    }
+    void Dash()
+    {
+        dashTimer -= Time.deltaTime;
+        if (dashTimer > 0)
+            return;
+        else isDashing = false;        
+        if(playerInput.DashInput)
+        {
+            isDashing = true;
+            anim.SetTrigger("dash");
+            dashTimer = dashCooldown;            
+            rb.AddForce(playerInput.DashForce * dashSpeed, ForceMode.VelocityChange);
+        }
+    }
+    void Movement()
+    {
+        if (isDashing)
+            return;
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float HorizontalMove { get; private set; }
+    public float VerticalMove { get; private set; }
+    public float HorizontalMoveRaw { get; private set; }
+    public float VerticalMoveRaw { get; private set; }
+    public bool DashInput { get; private set; }
+    public Vector3 DashForce { get; private set; }
+
+
+    private void Update()
+    {
+        HorizontalMove = Input.GetAxis("Horizontal");
+        HorizontalMoveRaw = Input.GetAxisRaw("Horizontal");
+        VerticalMove = Input.GetAxis("Vertical");
+        VerticalMoveRaw = Input.GetAxisRaw("Vertical");
+        DashForce = new Vector3(HorizontalMoveRaw, 0f, VerticalMoveRaw).normalized;
+        DashInput = Input.GetKeyDown(KeyCode.LeftShift);
+    }
+}
