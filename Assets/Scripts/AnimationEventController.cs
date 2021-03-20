593346028fb3461bf15dc75667943b785d90d845
Can LO <43052031+canlo33@users.noreply.github.com>
2021-03-20 00:25:45 -0400

    Melee Combo Attack Implementation
    1) Melee Attack functionality has been implemented with a combo system where player will have combo attacks when pressed attack input continuously. However, visual effects of the attack animations are yet to be implemented.
    
    2) Improvement over the Jump functionality were made so that the jump animation will play same through every device regardless of their frame rate. Also to make the feel of the jump animation more realistic. I added a code to increase the gravity one the player reach the peak of the jump.

---
+++ Assets/Scripts/AnimationEventController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventController : MonoBehaviour
+{
+    private Animator anim;
+    private PlayerController playerController;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        playerController = GetComponentInParent<PlayerController>();
+    }
+    void StartCombo(int amount)
+    {
+        if(playerController.comboTimerOn)
+        {
+            anim.SetInteger("comboCount", amount);
+            playerController.comboTimerOn = false;
+        }
+    }
+    void ResetCombo()
+    {
+        playerController.comboTimerOn = false;
+        playerController.comboCount = 0;
+        playerController.canMove = true;
+        anim.SetInteger("comboCount", 0);
+        
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -25,29 +26,41 @@ public class PlayerController : MonoBehaviour
     //Movement Related Variables
     public float movementSpeed;
     private bool isMoving;
-    private bool canMove = true;
+    public bool canMove = true;
 
     //Jump Related Variables
     private bool isJumping;
     public float jumpSpeed;
+    private float defaultGravity;
+    public float gravityMultiplier;
 
     //Rotation Related Variables
     public float rotationSpeed = 0.1f;
     private float smoothVelocity;
+
+    //Melee Attack Related Variables
+    public bool comboTimerOn;
+    public int comboCount;
+
     void Awake()
     {
         anim = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
         camera = Camera.main.transform;
+        defaultGravity = Physics.gravity.y;
     }
     // Update is called once per frame
     void Update()
     {
         AnimatorHandler();
         HandleDash();
-        Movement();
         Jump();
+        MeleeAttack();
+    }
+    private void FixedUpdate()
+    {
+        Movement();
     }
     void AnimatorHandler()
     {
@@ -56,6 +69,7 @@ public class PlayerController : MonoBehaviour
         isMoving = !(playerInput.HorizontalMove == 0f && playerInput.VerticalMove == 0f);
         anim.SetBool("idle", !isMoving);
         anim.SetBool("run", isMoving);
+        anim.SetBool("isGrounded", !isJumping);
         if (!isDashing)
         {
             anim.SetFloat("horizontalMoveRaw", playerInput.HorizontalMoveRaw);
@@ -67,8 +81,11 @@ public class PlayerController : MonoBehaviour
         dashTimer -= Time.deltaTime;
         if (isJumping)
             return;
+        // Start the Dash if the conditions are met.
         if (playerInput.DashInput && !isDashing && isMoving)
             OnStartDash();
+
+        // End the Dash when the dashDuration is over and reset the parameters.
         else if (Time.time - dashStartTime >= dashDuration && isDashing)
             OnEndDash();            
     }
@@ -76,12 +93,16 @@ public class PlayerController : MonoBehaviour
     {
         if (dashTimer > 0)
             return;
-        isDashing = true;
-        dashStartTime = Time.time;
+
+        //Start the visual effects
         dashVolume.weight = .4f;
         anim.SetTrigger("dash");
         effects[0].gameObject.SetActive(true);
         effects[0].Play();
+
+        // Set the parameters and add force to the player at corresponding direction.
+        isDashing = true;
+        dashStartTime = Time.time;
         dashTimer = dashCooldown;
         rb.AddForce(transform.forward * dashSpeed, ForceMode.VelocityChange);
     }
@@ -94,7 +115,7 @@ public class PlayerController : MonoBehaviour
     }
     void Movement()
     {        
-        if (isDashing && !canMove)
+        if (isDashing || !canMove)
             return;
         //Finding the Input Direction.
         Vector3 direction = new Vector3(playerInput.HorizontalMove, 0f, playerInput.VerticalMove).normalized;
@@ -102,35 +123,60 @@ public class PlayerController : MonoBehaviour
         //Calculating target angle and smoothingly rotating the player to target angle considering where the camera is looking at.
         if (isMoving)
         {
+            //Calculate and smooth the rotation angle.
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
             float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothVelocity, rotationSpeed);
             transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
-            //Seting the movement direction. 
-            moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            moveDirection.y = rb.velocity.y;            
-        }
-        else
-            moveDirection = new Vector3(0f, rb.velocity.y, 0f);
-        rb.velocity = moveDirection.normalized * movementSpeed;
 
+            //Seting the movement direction. 
+            moveDirection = (Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward).normalized * movementSpeed * Time.fixedDeltaTime;
+            rb.MovePosition(transform.position + moveDirection);
+        }
+        
     }
     void Jump()
     {
-        if (isDashing || isJumping)
+        if (isDashing)
             return;
-        if(playerInput.JumpInput)
+        // If conditions are met, play the jump animation and add force to player on vertical axis.
+        if(playerInput.JumpInput & !isJumping)
         {
             isJumping = true;
             anim.SetTrigger("jump");
-            Vector3 jumpForce = new Vector3(0f, jumpSpeed, 0f);
+            Vector3 jumpForce = new Vector3(0f, jumpSpeed * Time.fixedDeltaTime, 0f);
             rb.AddForce(jumpForce, ForceMode.VelocityChange);
         }
-    }
 
+        //If the player started falling, increase the gravity.
+        if(rb.velocity.y <=0 && isJumping)
+            Physics.gravity = new Vector3(0f, defaultGravity * gravityMultiplier, 0f);
+    }
+    void MeleeAttack()
+    {
+        if (playerInput.AttackInput)
+        {
+            if(comboCount > 0)
+            {
+                comboTimerOn = true;
+            }
+            else
+            {
+                canMove = false;
+                rb.velocity = Vector3.zero;
+                anim.SetTrigger("meleeAttack");
+                comboCount++;
+            }
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
+        // If player is touching the ground, set isJumping parameter to false
         if (collision.collider.CompareTag("Ground"))
+        {
             isJumping = false;
+            Physics.gravity = new Vector3(0f, defaultGravity, 0f);
+        }
+            
     }
 
 }
